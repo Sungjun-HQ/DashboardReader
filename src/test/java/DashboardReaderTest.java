7e5458049662c911031a92908fbe2d7260a5b6c7
Sungjun-HQ <hellquake@naver.com>
2020-12-10 22:53:56 +0900

    DashboardReaderTest Update

--- src/test/java/DashboardReaderTest.java
+++ src/test/java/DashboardReaderTest.java
@@ -16,34 +16,26 @@ class DashboardReaderTest {
         DashboardReader dashboardReader = new DashboardReader();
 
         // 접속한 github에서 태스트용 저장소로 이동
-        GHRepository repo = dashboardReader.connectGH().getRepository("Sungjun-HQ/DashboardReader/");
+//        GHRepository repo = dashboardReader.connectGH().getRepository("Sungjun-HQ/DashboardReader/");
+        GHRepository repo = dashboardReader.connectGH().getRepository("whiteship/live-study/");
 
         // 저장소에서 이슈 상태가 OPEN (ALL, CLOSED)
-        List<GHIssue> list = repo.getIssues(GHIssueState.OPEN);
+        List<GHIssue> list = repo.getIssues(GHIssueState.ALL);
+//        List<GHIssue> list = repo.getIssues(GHIssueState.OPEN);
 
-//        System.out.println("Issue list size: " + list.size());
         assertTrue(list.size() > 0);
 
         Map<String, Integer> map1 = new HashMap<>();
 
-        // 이름, 코맨트가 있을 경우에 한해서만 내용 읽고 횟수 저장후 계산
         for (GHIssue ghIssue : list) {
             GHUser user;
-            List<GHIssueComment> comments;
+            List<GHIssueComment> commentsList = ghIssue.getComments();
 
-//            ArrayList<String> userList = new ArrayList();
-//            System.out.println("list(" + i + "): " + list.get(i).getTitle());
-
-            // 해당 리스트의 코맨트 리스트
-            comments = ghIssue.getComments();
-
-            if (comments.size() > 0) {
-                for (GHIssueComment comment : comments) {
+            if (commentsList.size() > 0) {
+                for (GHIssueComment comment : commentsList) {
                     user = comment.getUser();
 
                     // 유저의 로그인 id 가져오기
-//                    System.out.println("user name: " + user.getLogin());
-//                    userList.add(user.getLogin());
                     if (map1.get(user.getLogin()) == null) {
                         map1.put(user.getLogin(), 1);
                     } else {
@@ -53,24 +45,19 @@ class DashboardReaderTest {
             }
         }
 
-        for (int i = 0; i < map1.size(); i++) {
-            System.out.println(map1.keySet() + " : " + map1.values());
+        // map1 출력
+        /*for (String key : map1.keySet()) {
+            int value = map1.get(key);
+            System.out.println(key + ", " + value);
+        }*/
+
+        // map1 소수점 출력
+        for (String i :
+                map1.keySet()) {
+            float progressFloat = map1.get(i);
+            progressFloat = progressFloat / 18 * 100;
+            System.out.print("name: " + i);
+            System.out.println(", progress: " + String.format("%.2f", progressFloat));
         }
-
-
-//        System.out.println("comments list size: " + comments.size());
-//        assertTrue(comments.size() > 0);
-
-//        for (GHIssueComment com : comments) {
-//            System.out.println(com.getBody());
-//        }
-
-//        System.out.println("comments body: " + comments.get(0).getBody());
-
-//        int i = 0;
-//        for (GHIssue event :
-//                list) {
-//            assertNotNull(event.getIssue());
-//        }
     }
 }
